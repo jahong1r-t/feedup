6ca6760bb21c55e31afaf8b55c3656fa56952c99
jahong1r-t <jahongirtorayev1507@gmail.com>
2025-04-17 15:08:07 +0500

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import bot.TelegramBot;
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+public class Main {
+    @SneakyThrows
+    public static void main(String[] args) {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new TelegramBot());
     }
 }
